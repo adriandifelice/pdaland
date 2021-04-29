8c409e23a7d7dbc22cd3eb5426647891064559c9
Adrian Di Felice <adrian.di.felice@gmail.com>
2021-04-29 15:52:11 -0400

    styles, image carousel

--- pages/index.js
+++ pages/index.js
@@ -12,21 +12,35 @@ export default function Home() {
     { name: "bakery.png" },
     { name: "sandwich.png" },
   ];
+  const [current, setCurrent] = useState(0)
   const [image, setImage] = useState(images[0].name);
-
   
+  function handleClick () {
+  
+    if (current === images.length - 1) {
+      setCurrent(0);
+      setImage(images[0].name);
+      return;
+    }
+    setCurrent(current + 1);
+    setImage(images[current + 1].name)
+    
+}
+  const newCurrent = current;
   return (
     <div className={styles.container}>
       <div className={styles.App}>
         <div className={styles.widget}>
-          <h1 className={styles.h1}>Pain d'Avignon</h1>
+          <h1 className={styles.h1}>Welcome!</h1>
+          <p >I work with Pain d'Avignon bringing high quality baked goods for you</p>
           <div className={styles.test}>
             <CustomButton url={url} />
           </div>
         </div>
-      </div>
       <div className={styles.primaryImg}>
         <img className={styles.imagen} src={image} alt="First slide" />
+        <button className={styles.arrow} onClick={()=> handleClick()}>Click</button>
+      </div>
       </div>
     </div>
   );
